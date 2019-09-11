90ce906415f50b0d038d4275096265a99b168d4f
onyeka <onynwamba@gmail.com>
2019-09-11 21:59:59 +0100

    Added education section

---
+++ src/components/Education.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Typewriter from 'components/Typewriter';
+import Scrollchor from 'react-scrollchor';
+
+
+class Hero extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="edu-container">
+                <Typewriter id="education" className="edu-title" text="Education" />
+                <div className="edu-description">
+                    <div className="edu-qmul">
+                        <table>
+                            <tr>
+                                <th>When</th>
+                                <th>Where</th>
+                                <th>Result</th>
+                            </tr>
+                            <tr>
+                                <td>{"2018 - Current"}</td>
+                                <td>{"BSc Computer Science - Queen Mary, University of London"}</td>
+                                <td>{"Expected Graduation: 2022"}</td>
+                            </tr>
+                            <tr>
+                                <td>{"2016 - 2018"}</td>
+                                <td>{"St Dominic's Sixth Form College"}</td>
+                                <td>{"AAB"}</td>
+                            </tr>
+                            <tr>
+                                <td>{"2012 - 2016"}</td>
+                                <td>{"The Sacred Heart Language College"}</td>
+                                <td>{"11 A*-A"}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Hero;
