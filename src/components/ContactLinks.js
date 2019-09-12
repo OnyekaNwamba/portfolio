ad5dac9ccaac6021357e4020497ac926448722db
onyeka <onynwamba@gmail.com>
2019-09-12 23:12:38 +0100

    Added contact links

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import github from 'images/github-icon.png';
 
 class ContactLinks extends React.PureComponent {
   render() {
     return (
-      <div className="contact-links">
+        <div className="contact-links">
         <span>
+
           <a rel="noopener" target="_blank" href="mailto:onynwamba@gmail.com">
             onynwamba@gmail.com
           </a>
@@ -13,12 +15,13 @@ class ContactLinks extends React.PureComponent {
           <a
             rel="noopener"
             target="_blank"
-            href="#"
+            href="https://www.linkedin.com/in/onyekanwamba"
           >
             LinkedIn
           </a>
         </span>
         <span>
+            <i className="fab fa-github"></i>
           <a rel="noopener" target="_blank" href="https://github.com/OnyekaNwamba">
             github
           </a>
