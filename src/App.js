a9fc6bb06f1a393ad042896679639a4165fa30aa
Onyeka Nwamba <onynwamba@gmail.com>
2021-09-17 20:20:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class Html extends Component {
                 dangerouslySetInnerHTML={{ __html: this.props.body }}
             />
             {this.props.postBodyComponents}
-            <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+//             <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
 //             <noscript>
 //                 <img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerPolicy="no-referrer-when-downgrade"/>
 //             </noscript>
